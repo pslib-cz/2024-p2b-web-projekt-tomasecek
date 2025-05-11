a5bd712bc1f5d3ef325b9786e29c46f2d1049b7f
tomasecek <jojobon@email.cz>
2025-05-11 21:47:01 +0200

    dasd

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: "/2024-p2b-web-projekt-tomasecek/", 
+  build: {
+    outDir: 'dist', // Výstupní složka pro buildované soubory
+  },
+});
