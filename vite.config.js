b803a6430dd8ca0e846e77e23f9781e46274c55b
tomasecek <jojobon@email.cz>
2025-05-11 22:01:29 +0200

    dsa

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: "/2024-p2b-web-projekt-tomasecek/", 
-  build: {
-    outDir: 'dist', // Výstupní složka pro buildované soubory
-  },
-});
