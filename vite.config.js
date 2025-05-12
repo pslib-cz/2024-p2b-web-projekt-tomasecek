3519e45adee0ca8f6650974fa3cdca5101f90b63
tomasecek <jojobon@email.cz>
2025-05-12 12:18:37 +0200

    dsad

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import vituum from 'vituum'
+import { defineConfig } from 'vite';
+import vituum from 'vituum';
 
-export default {
-    plugins: [
-      vituum({
-        pages: {
-          normalizeBasePath: true
-        }, 
-        imports: {
-            paths: ['./src/styles/*/**', './src/scripts/*/**']
-        }
-      })
-    ],
-  }
+export default defineConfig({
+  base: '/2024-p2b-web-projekt-tomasecek/', // Nastav základní cestu pro GitHub Pages
+  plugins: [
+    vituum({
+      pages: {
+        normalizeBasePath: true,
+      },
+      imports: {
+        paths: ['./src/styles/*/**', './src/scripts/*/**'],
+      },
+    }),
+  ],
+});
